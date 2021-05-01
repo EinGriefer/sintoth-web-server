549e87008f5f5e5ec7eafb8ee90c77f9f940fc5f
EinGriefer <simplecontent@outlook.com>
2021-05-01 11:27:44 +0200

    formatting

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include "httpd.h"
 
 #include "response/Response.h"
+#include <iostream>
 
 std::string httpd::handle(Request request) {
+    std::cout << "URL: " << request.getRoute() << "\n";
     std::string html = "<h1>hi</h1>";
     Response response(html);
     return response.generate();
 }
-
 
 int main() {
     httpd::start();
