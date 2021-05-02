65373b69cd8152ff1461e67333b024fa69ec7166
EinGriefer <simplecontent@outlook.com>
2021-05-02 08:08:51 +0200

    nothing special

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 std::string httpd::handle(Request request) {
     std::cout << "URL: " << request.getRoute() << "\n";
-    std::string html = "<h1>hi</h1>";
+    std::string html = "<h1>Test</h1>";
     Response response(html);
     return response.generate();
 }
